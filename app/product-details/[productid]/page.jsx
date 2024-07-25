0d4c88978c4d2fa8fe449326ebdcfef87ec1e670
Ayman ezz <aymanmae12@gmail.com>
2024-07-25 20:21:05 +0300

    most of product details

--- app/product-details/[productid]/page.jsx
+++ app/product-details/[productid]/page.jsx
@@ -4,21 +4,39 @@ import ProductApis from '@/app/_util/ProductApis'
 import React, { useEffect, useState } from 'react'
 import ProductBanner from '../_components/ProductBanner'
 import { ProductInfo } from '../_components/ProductInfo'
+import ProductList from '@/app/_components/ProductList'
+
 
 function ProductDetails({params}) {
-  const [ProductDetails , setProductDetails] = useState([])
+  const [ProductDetails , setProductDetails] = useState({})
+  const [productList , setProductList] = useState([])
   useEffect( () => {
     getProductById_()
-  },[])
+  },[params?.productid])
   const getProductById_ = () => {
-    ProductApis.getProductById(params?.productid).then( res => setProductDetails(res.data.data) )
+    ProductApis.getProductById(params?.productid).then( res => {
+      setProductDetails(res.data.data)
+      getProductByCategory_(res.data.data)
+    })
   }
+  const getProductByCategory_ = (product) => {
+    ProductApis.getProductByCategory(product?.attributes?.category).then( res =>{
+      setProductList(res?.data?.data)
+      console.log(res?.data?.data);
+    }
+    )
+  }
+  console.log(productList);
   return (
-    <div className=' px-10 md:px-28 py-12'>
+    <div className=' container-lg px-10 md:px-32 py-12'>
       <BreadCrumb />
-      <div className='flex flex-col justify-around mt-10 md:flex-row'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 gap-5 sm:gap-3 justify-around mt-10 '>
         <ProductBanner product={ProductDetails} />
         <ProductInfo product={ProductDetails} />
+      </div>
+      <div className='mt-24'>
+        <h2 className='mb-4 text-xl'>Similar Products</h2>
+        <ProductList productList={productList} />
       </div>
     </div>
   )
--- app/product-details/_components/ProductBanner.jsx
+++ app/product-details/_components/ProductBanner.jsx
@@ -5,9 +5,9 @@ const ProductBanner = ({product}) => {
       <Image
         src={product?.attributes?.banner?.data?.attributes?.url}
         width={400}
-        height={400}
+        height={233}
         alt="banner-card"
-        className=" rounded-lg object-cover" />
+        className="rounded-lg object-cover w-full xl:w-[85%] " />
     </div>
   )
 }
